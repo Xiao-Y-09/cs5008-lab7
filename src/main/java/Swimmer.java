72237f838ebd45cedb0b5897537b020a58041219
Xiao Yang <xyang094012@gmail.com>
2026-03-20 13:31:42 -0700

    Add files via upload

---
+++ src/main/java/Swimmer.java
@@ -0,0 +1,58 @@
+import java.util.List;
+
+public class Swimmer implements Comparable<Swimmer> {
+    private String name;
+    private List<Double> butterfly50mTimes;
+    private List<Double> backstroke50mTimes;
+    private List<Double> breaststroke50mTimes;
+    private List<Double> freestyle50mTimes;
+
+    public Swimmer(String name, List<Double> butterfly50mTimes, List<Double> backstroke50mTimes, List<Double> breaststroke50mTimes, List<Double> freestyle50mTimes) {
+        this.name = name;
+        this.butterfly50mTimes = butterfly50mTimes;
+        this.backstroke50mTimes = backstroke50mTimes;
+        this.breaststroke50mTimes = breaststroke50mTimes;
+        this.freestyle50mTimes = freestyle50mTimes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Double> getButterfly50mTimes() {
+        return butterfly50mTimes;
+    }
+
+    public List<Double> getBackstroke50mTimes() {
+        return backstroke50mTimes;
+    }
+
+    public List<Double> getBreaststroke50mTimes() {
+        return breaststroke50mTimes;
+    }
+
+    public List<Double> getFreestyle50mTimes() {
+        return freestyle50mTimes;
+    }
+
+    @Override
+    public int compareTo(Swimmer otherSwimmer) {
+        double thisSum = 0.0;
+
+        for (Double time : this.freestyle50mTimes) {
+            thisSum += time;
+        }
+
+        double thisAverage = thisSum / this.freestyle50mTimes.size();
+        double otherSum = 0.0;
+
+        for (Double time : otherSwimmer.getFreestyle50mTimes()){
+            otherSum += time;
+        }
+
+        double otherAverage = otherSum / otherSwimmer.getFreestyle50mTimes().size();
+
+        return Double.compare(thisAverage, otherAverage);
+    }
+
+}
